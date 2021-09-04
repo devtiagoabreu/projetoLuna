95b4375027969593e178de754affd21118ac9f1c
Tiago Abreu <devtiagoabreu@gmail.com>
2021-09-04 00:17:55 -0300

    Favoritar um profissional

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\UserFavorite;
+use App\Models\Professional;
+
 class UserController extends Controller
 {
     private $loggedUser;
@@ -20,6 +23,39 @@ class UserController extends Controller
         $info = $this->loggedUser;
         $info['avatar'] = url('media/avatars/'.$info['avatar']);
         $array['data'] = $info;
+
+        return $array;
+    }
+
+    public function toggleFavorite(Request $request) {
+        $array = ['error' => ''];
+
+        $id_professional = $request->input('professional');
+
+        $professional = Professional::find($id_professional);
+
+
+        if($professional) {
+            $fav = UserFavorite::select()
+                ->where('id_user', $this->loggedUser->id)
+                ->where('id_professional', $id_professional)   
+            ->first();
+
+            if($fav) {
+                //remover
+                $fav->delete();
+                $array['have'] = false;
+            } else {
+                //adicionar
+                $newFav = new UserFavorite();
+                $newFav->id_user = $this->loggedUser->id;
+                $newFav->id_professional = $id_professional;
+                $newFav->save();
+                $array['have'] = true;
+            }
+        } else {
+            $array['error'] = 'Profissional inexistente';
+        }
 
         return $array;
     }
