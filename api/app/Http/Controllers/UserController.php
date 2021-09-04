2e553b87d576c5eff4785fdbeecf706e591bf4f8
Tiago Abreu <devtiagoabreu@gmail.com>
2021-09-04 19:20:25 -0300

    Editando informações do usuário

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
+use App\Models\User;
 use App\Models\UserAppointments;
 use App\Models\UserFavorite;
 use App\Models\Professional;
@@ -111,6 +113,47 @@ class UserController extends Controller
         } else {
             $array['error'] = 'Não existem agendamentos';
         }
+
+        return $array;
+    }
+
+    public function update(Request $request) {
+        $array = ['error' => ''];
+
+        $rules = [
+            'name' => 'min:2',
+            'email' => 'email|unique:users',
+            'password' => 'same:password_confirm',
+            'password_confirm' => 'same:password'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()) {
+            $array['error'] = $validator->messages();
+            return $array;
+        }
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $password_confirm = $request->input('password_confirm');
+
+        $user = User::find($this->loggedUser->id);
+
+        if($name) {
+            $user->name = $name;
+        }
+
+        if($email) {
+            $user->email = $email;
+        }
+
+        if($password) {
+            $user->password = password_hash($password, PASSWORD_DEFAULT);
+        }
+
+        $user->save();
 
         return $array;
     }
